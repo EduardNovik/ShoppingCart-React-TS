9703e816296dbc084926b1a17808d61185e6665a
Eduard <eddnovik@gmail.com>
2023-05-02 19:54:23 +0200

    add photo

--- src/components/AboutItem.tsx
+++ src/components/AboutItem.tsx
@@ -1,10 +1,11 @@
 import { Stack } from 'react-bootstrap';
+import BasketPhoto from '../../public/imgs/ShoppingCartPhoto.png'
 export const AboutItem = () => {
     return (
         <>
             <h1 style={{marginBottom:"60px"}}>About</h1>
             <Stack style={{alignItems:'center', justifyContent:'space-between', flexDirection:'row'}}>
-                <img src="../../public/imgs/ShoppingCartPhoto.png" alt="ShoppingCartPhoto" style={{width:"40%"}}/>
+                <img src={BasketPhoto} alt="BasketPhoto" style={{width:"40%"}}/>
                 <h4  className="fs-3" style={{width:'35%', textAlign:'left', fontWeight:'300'}}>
                     Welcome to our Shopping Cart App! 
                     Our app, built with React and TypeScript, lets you easily add, remove, and track the total price of your products. 
--- src/components/HomeItem.tsx
+++ src/components/HomeItem.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Button, Stack } from 'react-bootstrap';
 import { Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import BagPhoto from '../../public/imgs/BagPhoto.png'
 
 export const HomeItem = () => {
     return (
         <Stack style={{flexDirection:'row', alignItems:'center',justifyContent:'space-between'}}>
-            <img src="../../public/imgs/BagPhoto.png" alt="bag" style={{width:'40%'}}/>
+            <img src={BagPhoto} alt="bag" style={{width:'40%'}}/>
             <Nav>
                 <Nav.Link to="/store" as={NavLink}>
                    <Button variant="danger">Get started on your shopping</Button>
